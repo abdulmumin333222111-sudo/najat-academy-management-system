24542a04152176efb70accf2085c1ca80984b984
abdulmumin333222111-sudo <abdulmumin333222111@gmail.com>
2026-08-22 05:21:28 +0600

    Create db.ts

---
+++ lib/db.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.DATABASE_URL;
+
+if (!MONGODB_URI) {
+  throw new Error('দয়া করে .env ফাইলে DATABASE_URL ডিফাইন করুন');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
